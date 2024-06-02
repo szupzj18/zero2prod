c74bcbb440c0d25a325d23649386c6ce76ab5e48
ChrisPan <szupzj18@outlook.com>
2024-06-03 04:14:56 +0800

    add deps. impl async main (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{ HttpServer, web, App };
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    HttpServer::new( || 
+        App::new()
+            .route("/", web::get().to(index)) // default route endpoint
+    ).bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+async fn index() {
+    // WIP: indexing
+    "Hello, world!"
 }
