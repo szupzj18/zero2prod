0fbc815fa8d4e7fa241b793acf56db36def13c50
ChrisPan <szupzj18@outlook.com>
2024-10-17 19:55:18 +0800

    add log tracer

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod configuration;
 pub mod startup;
 use actix_web::{ web, HttpResponse };
 use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -15,7 +16,14 @@ pub async fn health_check() -> HttpResponse {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-     match sqlx::query!(
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber",
+        request_id,
+        form.email,
+        form.name
+    );
+    match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -28,9 +36,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.get_ref())
     .await 
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => { 
+            log::info!("request_id {} - New Subscriber: {} details have been saved", request_id, form.email);
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("failed to execute query: {}", e);
+            log::error!("request_id {} - failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
@@ -44,4 +55,3 @@ pub async fn index(form: web::Form<FormData>) -> String {
 //     let name = req.match_info().get("name").unwrap_or("World");
 //     println!("recieved from req uri: {}, method {}", req.path(), req.method());
 //     format!("Hello, {}!", &name)
-// }
