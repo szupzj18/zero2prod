1377a295267813643e8d72de311e827383ba40a5
ChrisPan <szupzj18@outlook.com>
2024-10-18 18:08:30 +0800

    use tracer logger for better structure

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,6 @@ pub mod routes;
 pub mod configuration;
 pub mod startup;
 use actix_web::{ web, HttpResponse };
-use sqlx::PgPool;
-use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -13,38 +11,6 @@ pub struct FormData {
 
 pub async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
-}
-
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber",
-        request_id,
-        form.email,
-        form.name
-    );
-    match sqlx::query!(
-        r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
-        "#,
-        uuid::Uuid::new_v4(),
-        form.email,
-        form.name,
-        chrono::Utc::now()
-    )
-    .execute(pool.get_ref())
-    .await 
-    {
-        Ok(_) => { 
-            log::info!("request_id {} - New Subscriber: {} details have been saved", request_id, form.email);
-            HttpResponse::Ok().finish()
-        },
-        Err(e) => {
-            log::error!("request_id {} - failed to execute query: {:?}", request_id, e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
 }
 
 pub async fn index(form: web::Form<FormData>) -> String {
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
-use actix_web::{ web::{self}, HttpResponse };
-use serde_json::value;
+use actix_web::{ web, HttpResponse };
+use uuid::Uuid;
 use sqlx::PgPool;
+use tracing::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -10,7 +11,21 @@ pub struct FormData {
 
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    log::info!("Adding a new subscriber. name: {}, email: {}", form.name, form.email);
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "adding a new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
+    );
+
+    // enter the span 
+    // bad practice, it's a temporay solution for this.
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!(
+        "Saving subscriber details to the database"
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -22,13 +37,14 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         chrono::Utc::now()
     )
     .execute(pool.get_ref())
-    .await {
+    .instrument(query_span)
+    .await 
+    {
         Ok(_) => {
-            log::info!("Subscribed {} with email {}", form.name, form.email);
             HttpResponse::Ok().finish()
         },
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            tracing::error!("request_id {} - failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
